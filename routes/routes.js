c68071be3ef2034b719d8aabffb6ec6ff5a58a2f
Jobayer-Majumder <jobayermojumder70@gmail.com>
2021-09-17 00:08:22 +0600

    fi

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,39 @@
 const express = require('express');
+const { usersCollection } = require('../dbCollection/dbCollection');
 const router = express.Router();
+
+router.post('/login', (req, res) => {
+    const { email, pass } = req.body;
+    usersCollection.find({ email, pass })
+        .toArray((err, document) => {
+            res.send(document[0])
+        })
+});
+
+
+router.post('/signup', async (req, res) => {
+    const { email, name, pass } = await req.body;
+    usersCollection.find({ email, name })
+        .toArray((err, document) => {
+            if (document.length > 0) {
+                res.json({errMessage: 'User already created !'})
+            } else {
+                usersCollection.insertOne({ name, email, pass })
+                    .then(result => {
+                        if(result.insertedId){
+                            res.json({sucMessage: 'User Created Successfully'})
+                        }
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    });
+            }
+        })
+
+
+    console.log(req.body)
+})
+
 
 router.get('/a', (req, res) => {
     res.send('look mom i am using express')
